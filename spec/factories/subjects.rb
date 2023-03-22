3a82175a4f689fa8681420e18630a290d44ff1ec
Kaja Witek <kaja.wasiolek@gmail.com>
2023-03-22 20:23:29 +0100

    test: update subjects factory

--- spec/factories/subjects.rb
+++ spec/factories/subjects.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :subject do
-    title { Faker::Science.element_subcategory }
+    sequence(:title) { |t| "#{Faker::Science.element_subcategory} #{t}" }
+
     description { Faker::Lorem.paragraph }
     user
   end
