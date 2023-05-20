8acf21137e12e237a09166f5ee3108ee83a16c65
Kaja Witek <kaja.wasiolek@gmail.com>
2023-05-20 19:30:07 +0200

    feat: add creating subjects

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class SubjectsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    render :index, locals: { subjects: Subject.all }
+  end
+
+  def new
+    render :new, locals: { subject: Subject.new }
+  end
+
+  def create
+    subject = Subject.new(subject_params)
+    subject.user = current_user
+    if subject.save
+      redirect_to subjects_path
+    else
+      render :new, locals: { subject: subject }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:title, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+
+  resources :subjects, only: %i[index new create]
 end
