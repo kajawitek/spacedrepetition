9a972f133de71b0a0609a39d54d4e1a88fe7d10f
Kaja Witek <kaja.wasiolek@gmail.com>
2023-05-21 18:37:20 +0200

    test: update subjects.spec

--- spec/system/subjects_spec.rb
+++ spec/system/subjects_spec.rb
@@ -9,14 +9,9 @@ RSpec.describe 'Subjects' do
     user = create(:user)
     subject = build(:subject)
 
+    login_as user, scope: :user
+
     visit root_path
-    click_link 'Log In'
-
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-
-    click_button 'Log in'
-    expect(page).to have_content('Signed in successfully.')
 
     expect(Subject.all.count).to eq(0)
 
@@ -24,37 +19,47 @@ RSpec.describe 'Subjects' do
     click_link 'New Subject'
 
     fill_in 'Title', with: subject.title
-
     click_button 'Create Subject'
 
     expect(page).to have_content('Subject was successfully created.')
+    expect(page).to have_content(subject.title)
     expect(Subject.all.count).to eq(1)
   end
 
   it 'updates subject' do
     user = create(:user)
-    subject = create(:subject)
+    create(:subject)
+
+    login_as user, scope: :user
 
     visit root_path
-    click_link 'Log In'
-
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-
-    click_button 'Log in'
-    expect(page).to have_content('Signed in successfully.')
 
     expect(Subject.all.count).to eq(1)
 
     click_link 'Subjects'
     click_link 'Edit'
-
     fill_in 'Title', with: 'New Title'
-
     click_button 'Update Subject'
 
     expect(page).to have_content('Subject was successfully updated.')
     expect(Subject.all.count).to eq(1)
     expect(Subject.first.title).to eq('New Title')
+  end
+
+  it 'destroys subject' do
+    user = create(:user)
+    create(:subject)
+
+    login_as user, scope: :user
+
+    visit root_path
+
+    expect(Subject.all.count).to eq(1)
+
+    click_link 'Subjects'
+    click_link 'Destroy'
+
+    expect(page).to have_content('Subject was successfully destroyed.')
+    expect(Subject.all.count).to eq(0)
   end
 end
